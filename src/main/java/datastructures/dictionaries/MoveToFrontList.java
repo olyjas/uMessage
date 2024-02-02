4f605a123368296fb4fb9271a3713b01841212f8
Jasmine <jaszhang@cs.washington.edu>
2024-02-01 20:17:21 -0800

    movetofrontlist finished

--- src/main/java/datastructures/dictionaries/MoveToFrontList.java
+++ src/main/java/datastructures/dictionaries/MoveToFrontList.java
@@ -3,8 +3,10 @@ package datastructures.dictionaries;
 import cse332.datastructures.containers.Item;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
+import cse332.interfaces.misc.SimpleIterator;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 1. The list is typically not sorted.
@@ -20,18 +22,94 @@ import java.util.Iterator;
  * dictionary/list's iterator.
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
+    private ListItem front;
+
+    private class ListItem {
+        private Item<K, V> data;
+        private ListItem next;
+
+        public ListItem() {
+            this(null, null);
+        }
+
+        public ListItem(Item<K, V> item) {
+            this(item, null);
+        }
+
+        public ListItem(Item<K, V> item, ListItem next) {
+            this.data = item;
+            this.next = next;
+        }
+    }
+
     @Override
     public V insert(K key, V value) {
-        throw new NotYetImplementedException();
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        V previous = this.find(key);
+        if (previous != null) {
+            this.front.data.value = value;
+        } else {
+            this.front = new ListItem(new Item<>(key, value), this.front);
+            this.size++;
+        }
+        return previous;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        ListItem curr = this.front;
+        V value = null;
+        if (curr != null && curr.data != null) {
+            if (curr.data.key.equals(key)) {
+                return curr.data.value;
+            }
+            while (curr.next != null && curr.next.data != null &&
+                    !curr.next.data.key.equals(key)) {
+                curr = curr.next;
+            }
+            if (curr.next != null && curr.next.data != null) {
+                value = curr.next.data.value;
+                ListItem nodeToMoveFront = curr.next;
+                curr.next = nodeToMoveFront.next;
+                nodeToMoveFront.next = this.front;
+                this.front = nodeToMoveFront;
+            }
+            return value;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
-        throw new NotYetImplementedException();
+        return new ListIterator();
+    }
+
+    private class ListIterator extends SimpleIterator<Item<K, V>> {
+        private ListItem curr;
+
+        public ListIterator() {
+            if (front != null) {
+                this.curr = front;
+            }
+        }
+
+        public boolean hasNext() {
+            return this.curr != null;
+        }
+
+        public Item<K, V> next() {
+            if (this.curr == null) {
+                throw new NoSuchElementException();
+            }
+            Item<K, V> value = new Item<K, V>(this.curr.data.key, this.curr.data.value);
+            this.curr = this.curr.next;
+            return value;
+        }
     }
 }
