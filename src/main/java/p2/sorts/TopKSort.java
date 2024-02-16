634d5b308594a936fa9cf212f6c17a584c6ef67b
Jasmine <jaszhang@cs.washington.edu>
2024-02-15 20:58:29 -0800

    topk

--- src/main/java/p2/sorts/TopKSort.java
+++ src/main/java/p2/sorts/TopKSort.java
@@ -14,27 +14,20 @@ public class TopKSort {
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
         if (k > array.length) {
             k = array.length;
-        } else if (array.length == 0) {
-            return;
         }
-
-        MinFourHeap<E> heap = new MinFourHeap<>(comparator);
+        MinFourHeap<E> sortingHeap = new MinFourHeap<E>(comparator);
         for (int i = 0; i < k; i++) {
-            heap.add(array[i]);
+            sortingHeap.add(array[i]);
         }
-
-
-
         for (int i = k; i < array.length; i++) {
-            if (comparator.compare(array[i], heap.peek()) > 0) {
-                heap.next();
-                heap.add(array[i]);
+            if (comparator.compare(array[i], sortingHeap.peek()) > 0) {
+                sortingHeap.next();
+                sortingHeap.add(array[i]);
             }
             array[i] = null;
         }
-
         for (int i = 0; i < k; i++) {
-            array[i] = heap.next();
+            array[i] = sortingHeap.next();
         }
     }
 }
